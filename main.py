117f0d30c879ab2a7622f6821e6b8f59d9e89536
Martin Novice <96988126+Novicce@users.noreply.github.com>
2022-03-16 13:19:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ ODDELOVAC = ("=" * 79)
 
 print(ODDELOVAC)
 
+from traceback import print_tb
 from users import user_bob
 from users import user_ann
 from users import user_liz
 from users import user_mike
+from task_template import TEXTS
 
 #===================================================
 #vstup name a password
@@ -17,15 +19,24 @@ uvitaci_obrazovka = ("Welcome to the app,", uzivatel, "We have 3 texts to be ana
 #===================================================
 #podminky prihlaseni
 
-if uzivatel == "bob" and heslo == user_bob:
-    print("Vítej Bob")
-elif uzivatel == "ann" and heslo == user_ann:
-    print("Vítej Ann")
-elif uzivatel == "mike" and heslo == user_mike:
-    print("Vítej Mike")
-elif uzivatel == "liz" and heslo == user_liz:
-    print("Vítej Liz")
+if uzivatel == "Bob" and heslo == user_bob:
+    print(ODDELOVAC)
+    print(f"Welcome to the app, {uzivatel}.")
+    print("We have 3 texts to be analyzed.")
+elif uzivatel == "Ann" and heslo == user_ann:
+    print("Welcome to the app,", uzivatel,".")
+    print("We have 3 texts to be analyzed.")
+elif uzivatel == "Mike" and heslo == user_mike:
+    print("Welcome to the app,", uzivatel,".")
+    print("We have 3 texts to be analyzed.")
+elif uzivatel == "Liz" and heslo == user_liz:
+    print("Welcome to the app,", uzivatel,".")
+    print("We have 3 texts to be analyzed.")
 else:
-  print("neregistrovaný uživatel")
+  print("unregistered user, terminating the program..")
+  exit()
+print(ODDELOVAC)
+print("Enter a number btw. 1 and 3 to select: 1")
 print(ODDELOVAC)
 #===================================================
+
