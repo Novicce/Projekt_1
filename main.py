4faa71d53673e2f56b33e4dc156cb434fe4f482a
Martin Novice <96988126+Novicce@users.noreply.github.com>
2022-03-16 12:02:20 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ ODDELOVAC = ("=" * 79)
 
 print(ODDELOVAC)
 
+from users import user_bob
+from users import user_ann
+from users import user_liz
+from users import user_mike
+
 #===================================================
 #vstup name a password
 
 uzivatel = input("username: ")
 heslo = input("password: ")
-uvitaci_obrazovka = "Vítejte"
+uvitaci_obrazovka = ("Welcome to the app,", uzivatel, "We have 3 texts to be analyzed.")
 
 #===================================================
 #podminky prihlaseni
 
-if uzivatel == "Martin":
-    print("Vitej!")
+if uzivatel == "bob" and heslo == user_bob:
+    print("Vítej Bob")
+elif uzivatel == "ann" and heslo == user_ann:
+    print("Vítej Ann")
+elif uzivatel == "mike" and heslo == user_mike:
+    print("Vítej Mike")
+elif uzivatel == "liz" and heslo == user_liz:
+    print("Vítej Liz")
 else:
-    print("neregistrovaný uživatel")
-
-if heslo == int(243567):
-    print(uvitaci_obrazovka)
-
-
+  print("neregistrovaný uživatel")
+print(ODDELOVAC)
+#===================================================
