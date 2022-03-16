5d3e174dd81bd2192be7e4322695d788ed1d5457
Martin Novice <96988126+Novicce@users.noreply.github.com>
2022-03-16 10:47:01 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 ODDELOVAC = ("=" * 79)
 
 print(ODDELOVAC)
-print("Hello World!")
+
+#===================================================
+#vstup name a password
+
+uzivatel = input("username: ")
+heslo = input("password: ")
+uvitaci_obrazovka = "Vítejte"
+
+#===================================================
+#podminky prihlaseni
+
+if uzivatel == "Martin":
+    print("Vitej!")
+else:
+    print("neregistrovaný uživatel")
+
+if heslo == int(243567):
+    print(uvitaci_obrazovka)
+
+
